b5c09e069caefea977b0d0b959b46183a978952e
Kitae's Github <aqua0405@gmail.com>
2025-12-29 15:11:06 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import NavBar from "./components/NavBar"
 import Footer from "./components/Footer"
 import Home from "./pages/Home"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
 
 function App() {
 
@@ -12,6 +14,9 @@ function App() {
                 {/* 일반 라우터 */}
                 <Route path="/home" element={<Home />} />
                 <Route path="/" element={<Home />} />
+
+                <Route path="/register" element={<Register />} />    
+                <Route path="/login" element={<Login />} />    
 
             </Routes>
             <Footer />
